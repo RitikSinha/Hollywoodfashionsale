f525a695348a557fef978cfdd7b60ff0a48421f6
ritiksinha <ritikk910@gmail.com>
2022-12-29 00:37:22 +0530

    privacy policy done

---
+++ pages/contact.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../components/Layout";
+const contact = () => {
+  return (
+    <Layout title="contact">
+      <h1>contact page</h1>
+    </Layout>
+  );
+};
+
+export default contact;
---
+++ pages/refundPolicy.jsx
@@ -0,0 +1,147 @@
+import React from "react";
+import Layout from "../components/Layout";
+const refundPolicy = () => {
+  return (
+    <Layout title="Return and Refund Policy for Hollywood Fashion Sale">
+      <h1>Return and Refund Policy</h1>
+      <p>Last updated: December 28, 2022</p>
+      <p>Thank you for shopping at Hollywood Fashion Sale.</p>
+      <p>
+        If, for any reason, You are not completely satisfied with a purchase We
+        invite You to review our policy on refunds and returns. This Return and
+        Refund Policy has been created with the help of the Free Return and
+        Refund Policy Generator.
+      </p>
+      <p>
+        The following terms are applicable for any products that You purchased
+        with Us.
+      </p>
+      <h2>Interpretation and Definitions</h2>
+      <h3>Interpretation</h3>
+      <p>
+        The words of which the initial letter is capitalized have meanings
+        defined under the following conditions. The following definitions shall
+        have the same meaning regardless of whether they appear in singular or
+        in plural.
+      </p>
+      <h3>Definitions</h3>
+      <p>For the purposes of this Return and Refund Policy:</p>
+      <ul>
+        <li>
+          <strong> Company</strong> (referred to as either &quot; the Company
+          &quot;, &quot;We&quot;, &quot;Us&quot; or &quot;Our&quot; in this
+          Agreement) refers to Hollywood Fashion Sale LLC, Gautam Buddha Road,
+          Chowk, Gaya - 823001, Kedarnath Market.
+        </li>
+        <li>
+          {" "}
+          <strong>Goods</strong> refer to the items offered for sale on the
+          Service.
+        </li>
+        <li>
+          {" "}
+          <strong>Orders</strong> mean a request by You to purchase Goods from
+          Us.
+        </li>
+        <li>
+          {" "}
+          <strong> Service</strong> refers to the Website.
+        </li>
+        <li>
+          <strong>Website</strong> refers to Hollywood Fashion Sale, accessible
+          from{" "}
+          <a href="https://hollywoodfashionsale.com">hollywoodfashionsale</a>
+        </li>
+        <li>
+          <strong>You</strong> means the individual accessing or using the
+          Service, or the company, or other legal entity on behalf of which such
+          individual is accessing or using the Service, as applicable.
+        </li>
+      </ul>
+      <h2>Your Order Cancellation Rights</h2>
+      <p>
+        You are entitled to cancel Your Order within 7 days without giving any
+        reason for doing so.
+      </p>
+      <p>
+        The deadline for cancelling an Order is 7 days from the date on which
+        You received the Goods or on which a third party you have appointed, who
+        is not the carrier, takes possession of the product delivered.
+      </p>
+      <p>
+        In order to exercise Your right of cancellation, You must inform Us of
+        your decision by means of a clear statement. You can inform us of your
+        decision by:
+        <ul>
+          <li>By phone number: +91 8292995048</li>
+        </ul>
+        <p>
+          We will reimburse You no later than 14 days from the day on which We
+          receive the returned Goods. We will use the same means of payment as
+          You used for the Order, and You will not incur any fees for such
+          reimbursement.
+        </p>
+      </p>
+      <h2>Conditions for Returns</h2>
+      <p>
+        In order for the Goods to be eligible for a return, please make sure
+        that:
+        <ul>
+          <li>The Goods were purchased in the last 7 days</li>
+          <li>The Goods are in the original packaging</li>
+        </ul>
+        <p>
+          The following Goods cannot be returned:
+          <ul>
+            <li>
+              The supply of Goods made to Your specifications or clearly
+              personalized.
+            </li>
+            <li>
+              The supply of Goods which according to their nature are not
+              suitable to be returned, deteriorate rapidly or where the date of
+              expiry is over.
+            </li>
+            <li>
+              The supply of Goods which are not suitable for return due to
+              health protection or hygiene reasons and were unsealed after
+              delivery.
+            </li>
+            <li>
+              The supply of Goods which are, after delivery, according to their
+              nature, inseparably mixed with other items.
+            </li>
+          </ul>
+          <p>
+            We reserve the right to refuse returns of any merchandise that does
+            not meet the above return conditions in our sole discretion.
+          </p>
+          <p>
+            Only regular priced Goods may be refunded. Unfortunately, Goods on
+            sale cannot be refunded. This exclusion may not apply to You if it
+            is not permitted by applicable law.
+          </p>
+        </p>
+      </p>
+      <h2>Returning Goods</h2>
+      <p>
+        You are responsible for the cost and risk of returning the Goods to Us.
+        You should send the Goods at the following address:
+      </p>
+      <p>Gautam Buddha Road, Chowk, Gaya - 823001, Kedarnath Market</p>
+      <p>
+        We cannot be held responsible for Goods damaged or lost in return
+        shipment. Therefore, We recommend an insured and trackable mail service.
+        We are unable to issue a refund without actual receipt of the Goods or
+        proof of received return delivery.
+      </p>
+      <h2>Contact Us</h2>
+      <p>
+        If you have any questions about our Returns and Refunds Policy, please
+        <li>contact us: By phone number: +91 8292995048</li>
+      </p>
+    </Layout>
+  );
+};
+
+export default refundPolicy;
